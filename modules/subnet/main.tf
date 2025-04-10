c244e64b62d2ffca714650ea2f0f05fc1a2e006f
sudheer <sudheersunny129@gmail.com>
2025-04-10 15:32:01 +0530

    Fix module VPC reference error
    - Replace direct reference to aws_vpc.myapp-vpc.id with var.vpc_id in modules/subnet/main.tf.

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,7 +1,7 @@
 # Creating a subnet in the specified Availability Zone within the custom VPC
 resource "aws_subnet" "myapp-subnet-1" {
-    vpc_id = aws_vpc.myapp-vpc.id
-    cidr_block = var.vpc_id
+    vpc_id = var.vpc_id
+    cidr_block = var.subnet_cidr_block
     availability_zone = var.avail_zone
     tags = {
         Name = "${var.env_prefix}-subnet-1"
